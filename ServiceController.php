56ce5a2528099dc1bd6dd438a55827ac5d5c45cf
amine-moustaqssa <75173774+amine-moustaqssa@users.noreply.github.com>
2025-09-19 22:10:00 +0100

    Add files via upload

---
+++ ServiceController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class ServiceController extends AbstractController
+{
+    #[Route('/service', name: 'app_service')]
+    public function index(): Response
+    {
+        return $this->render('service/index.html.twig', [
+            'controller_name' => 'ServiceController',
+        ]);
+    }
+    #[Route('/showService/{nom}', name: 'app_Service')]
+    public function showService($nom) 
+    {
+        return $this->render("service/affiche.html.twig",[            
+            'nom' => $nom,
+        ]);
+
+    }
+
+}
